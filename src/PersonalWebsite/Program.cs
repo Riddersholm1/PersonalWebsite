8e6e467afc932d6d23518343efe85ffa026d4f4c
Riddersholm97 <jesper.riddersholm@sydbank.dk>
2025-11-13 10:50:32 +0100

    bump to dotnet 10

--- src/PersonalWebsite/Program.cs
+++ src/PersonalWebsite/Program.cs
@@ -1,10 +1,9 @@
 using Blazorise;
 using Blazorise.Icons.FontAwesome;
 using Blazorise.Tailwind;
-using PersonalWebsite;
-using PersonalWebsite.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using PersonalWebsite;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -18,7 +17,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddSingleton<ThemeService>();
-
-await builder.Build()
-    .RunAsync();
+await builder.Build().RunAsync();
