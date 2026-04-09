ddeece9df12aba8d5669912ca28c28f6e211fd8a
Jesper Bruhn Riddersholm <jrn6270@hotmail.com>
2026-04-09 08:36:47 +0200

    Strip section nav from top bar (#9)

--- MyWebsite/Layout/MainLayout.razor.cs
+++ MyWebsite/Layout/MainLayout.razor.cs
@@ -1,45 +1,8 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
-
 namespace MyWebsite.Layout;
 
-public partial class MainLayout : IAsyncDisposable
+public partial class MainLayout
 {
-    [Inject]
-    private IJSRuntime JS { get; set; } = null!;
-
     private bool _isDarkMode = true;
-    private string _activeSection = "home";
-    private DotNetObjectReference<MainLayout>? _dotNetRef;
-
-    private bool IsActiveSection(string sectionId) =>
-        string.Equals(_activeSection, sectionId, StringComparison.OrdinalIgnoreCase);
 
     private void ToggleTheme() => _isDarkMode = !_isDarkMode;
-
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        if (firstRender)
-        {
-            _dotNetRef = DotNetObjectReference.Create(this);
-            await JS.InvokeVoidAsync("sectionObserver.init", _dotNetRef);
-        }
-    }
-
-    [JSInvokable]
-    public void SetActiveSection(string sectionId)
-    {
-        _activeSection = sectionId;
-        StateHasChanged();
-    }
-
-    public async ValueTask DisposeAsync()
-    {
-        if (_dotNetRef is not null)
-        {
-            await JS.InvokeVoidAsync("sectionObserver.dispose");
-            _dotNetRef.Dispose();
-            GC.SuppressFinalize(this);
-        }
-    }
 }
